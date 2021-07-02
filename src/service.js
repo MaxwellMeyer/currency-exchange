eaa3eae660970a290b8e1194851997230d2331ac
Maxwell Meyer <maxreadswell@gmail.com>
2021-07-02 11:12:49 -0700

    begin writing api service logic

---
+++ src/service.js
@@ -0,0 +1,7 @@
+export default class CurrencyExchange {
+  static async getExchangeRate() {
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+    }
+  }
+}
