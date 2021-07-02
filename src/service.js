5bf2ad468b439b046d7e12cae0096520e7bb1e5b
Maxwell Meyer <maxreadswell@gmail.com>
2021-07-02 11:58:42 -0700

    finish api service logic, add catch error message

--- src/service.js
+++ src/service.js
@@ -1,7 +1,15 @@
 export default class CurrencyExchange {
   static async getExchangeRate() {
     try {
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+      const response = await fetch(
+        `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`
+      );
+      if (!response.ok) {
+        throw Error(response);
+      }
+      return response.json();
+    } catch (error) {
+      return `Unfortunately, we were unable to complete the request due to ${error}`;
     }
   }
 }
