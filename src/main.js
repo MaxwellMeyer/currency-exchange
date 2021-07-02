72d55346fa42f389499013df0f7824b1239d2a04
Maxwell Meyer <maxreadswell@gmail.com>
2021-07-02 12:50:00 -0700

    begin ui logic, break 4 lunch

--- src/main.js
+++ src/main.js
@@ -3,3 +3,11 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 import CurrencyExchange from "../src/service.js";
+
+$(document).ready(function () {
+  $("#exchange").submit(function (event) {
+    event.preventDefault();
+    let usd = parseInt($("#amount").val());
+    let forex = $("#country").val();
+  });
+});
