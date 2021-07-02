3ef17e462d5d1afc2cdcac5584e624c0fc280dc4
Maxwell Meyer <maxreadswell@gmail.com>
2021-07-02 14:49:22 -0700

    finish building calcExchange(), add to ui logic

--- src/main.js
+++ src/main.js
@@ -4,12 +4,31 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 import CurrencyExchange from "../src/service.js";
 
-async function calcExchange(usd, forex) {}
+async function calcExchange(usd, forex) {
+  let response = await CurrencyExchange.getExchangeRate();
+  if (response.result === "success") {
+    const rate = (usd * response.conversion_rates[forex]).toFixed(1);
+    if (isNaN(rate)) {
+      $(".results").text(`Please enter a valid currency to compare rates!`);
+    } else if (isNaN(usd)) {
+      $(".results").text(
+        `Please enter a valid numerical currency amount to compare!`
+      );
+    } else if (typeof response === "string") {
+      $(".results").html(`${response}`);
+    } else {
+      $(".results").text(`$${usd} converts to ${rate} ${forex}`);
+    }
+  }
+}
 
 $(document).ready(function () {
   $("#exchange").submit(function (event) {
     event.preventDefault();
     let usd = parseInt($("#amount").val());
     let forex = $("#country").val();
+    calcExchange(usd, forex);
+    $(".results").show();
+    $("#country").val("");
   });
 });
