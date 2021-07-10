d677c2c51daf4c34eb8c9ad346982627114fe1b0
Maxwell Meyer <maxreadswell@gmail.com>
2021-07-09 21:18:28 -0700

    update error message for non 200ok response

--- src/service.js
+++ src/service.js
@@ -5,11 +5,12 @@ export default class CurrencyExchange {
         `https://v6.exchangerate-api.com/v6/f77fc57ce905a65/latest/USD`
       );
       if (!response.ok) {
+        console.log(response);
         throw Error(response);
       }
       return response.json();
-    } catch (error) {
-      return `Unfortunately, we were unable to complete the request due to ${error}`;
+    } catch (Error) {
+      return `Unfortunately, we were unable to complete the request due to a network error.`;
     }
   }
 }
