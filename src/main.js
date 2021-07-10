fec11e28eb61f29f057838526f5f345495fcc77c
Maxwell Meyer <maxreadswell@gmail.com>
2021-07-10 13:12:12 -0700

    add logic to insert commas in larger numbers

--- src/main.js
+++ src/main.js
@@ -4,19 +4,26 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 import CurrencyExchange from "../src/service.js";
 
+function addComm(num) {
+  return num.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+}
+
 async function calcExchange(usd, forex) {
   let response = await CurrencyExchange.getExchangeRate();
   if (response.result === "success") {
-    const rate = (usd * response.conversion_rates[forex]).toFixed(1);
+    const rate = (usd * response.conversion_rates[forex]).toFixed(2);
+    const commaNum = addComm(rate);
+    const usdComm = addComm(usd);
     if (isNaN(rate)) {
       $(".results").text(`Please select a valid currency to compare rates!`);
     } else {
-      $(".results").text(`$${usd} USD converts to ${rate} ${forex}!`);
+      $(".results").text(`$${usdComm} USD converts to ${commaNum} ${forex}!`);
     }
   } else if (typeof response === "string") {
     $(".results").html(`${response}`);
   }
 }
+
 $(document).ready(function () {
   $("#exchange").submit(function (event) {
     event.preventDefault();
